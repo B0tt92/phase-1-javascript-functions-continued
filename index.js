e03cf5458477dcc5b83e1c5255432b5d1db645e1
+branden <+branden_ott@outlook.com+>
2021-11-03 19:14:45 -0400

    functions cont

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+
+
+};
+function mondayWork(activity ='go to the office') {
+    return `This Monday, I will ${activity}.`
+};
+function wrapAdjective(param = "*") {
+    let str = "You are "
+    let str2 = "||a dedicated programmer||!"
+    let str3 = "*a hard worker*!"
+        return function() {
+            let special = param
+            if (special === "*") {
+                return `${str}${str3}`
+            }else if (special === "||") {
+                return `${str}${str2}`
+            }else return `${str}"special`
+        }
+
+};
